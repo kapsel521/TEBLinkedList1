240f2ae2ce304651ad60999789174c24f9da12fc
Marek <kapsel521@gmail,com>
2020-11-27 15:39:28 +0100

    Initial commit

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class LinkedList {
+    private Node pierwszy; //pierwszy element listy
+    private Node ostatni;  //ostatni element listy
+
+    //TODO(2) Tworzymy metodę dodajOstatni, która będzie miała jeden parametr - dodawany element
+        //TODO(2a) Inicjalizujemy noy element klasy node, który bedziemy dodawać
+        //Todo(2b) Sprawdzamy instrukcją warunkową, czy lista jest pusta, zapisujemy nowy element
+
+    //TODO(3) Tworzymy metodę wydrukuj, używając pętli while
+        //TODO(3a) Inicjalizujemy nowy element klasy Node
+        //TODO(3b) pętlę while zaczynamy od elementu pierwszego i drukując, następnie przechodzimy do kolejnego elementu, aż do końca listy
+
+    //TODO(4) Tworzymy metodę dodajPierwszy, która będzie miała jeden parametr - dodawany element
+        //TODO(4a) Inicjalizujemy nowy element klasy Node
+        //TODO(4b) Sprawdzamy instrukcją warunkową, czy lista jest pusta, zapisujemy nowy element, pamiętamy o przypisaniu adresu do komórki która była pierwsza
+
+    //TODO(5) Tworzymy metodę jakiIndex, która będzie miała jeden parametr - wawrtośc szukanego indexu
+        //TODO(5a) Tworzymy zmienną prymitywną int index, która na początku ma wartość 0
+        //TODO(5b) Tworzymy pętle while, która iteruje od początku po elemeentach listy, oraz tworzymy instrukcję warunkową sprawdzającą wartości
+        //TODO(5c) W przypadku braku dopasowania funkcja ma zwrócić wartość -1
+
+    //TODO(6) Tworzymy metodę czyZawiera, która będzie miała jeden parametr - sprawdzany element
+        //TODO(6a) Spore analogie do metody jakiIndex
+
+    //TODO(7) Tworzymy metodę usuńPierwszy - metoda bezparametrowa
+        //TODO(7a) Tworzymy element klasy Node
+        //TODO(7b) Instrukcja warunkowa, czy lista pusta, czy ma jeden element, dla reszty przypadków
+
+    //TODO(8) Tworzymy metodę usuńOstatni - metoda bezparametrowa
+        //TODO(8a) Spore analogie do metody usuńPierwszy
+
+}
+
---
+++ src/com/company/Node.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Node {
+    public int value;  //pole z wartością przetrzymywaną w liście
+    public Node next;  //pole z adresem następnej wartości
+
+    //TODO(1) tworzymy konstruktor klasy z wartością value
+}
